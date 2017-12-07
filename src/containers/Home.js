1343e7c6f1e2f0539dd01ddbee471fd87957f82c
sash man <sashman90@gmail.com>
2017-12-07 23:39:55 +0000

    Using responsive css grid to get a bit closer

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,31 +4,90 @@ import glamorous from 'glamorous'
 
 const Background = glamorous.div( ({backgroundImageUrl}) => ({
   width: "100%",
-  height: "1152px",
-  backgroundImage: "url(" + backgroundImageUrl + ")"
+  height: "100vh",
+  background: "url(" + backgroundImageUrl + ") center/cover"
 }))
+
+const Grid = glamorous.div({
+  // You can use @supports with glamor!
+  // So you can use @supports with glamorous as well!
+
+  'marginTop': '0px',
+  '@media (min-width: 700px)': {
+    '@supports (display: grid)': {
+      // 'grid-template-columns': '450px auto auto',
+      'grid-template-rows': 'auto',
+      display: 'grid',
+      gridGap: 5,
+      gridTemplateAreas: `
+      ". title ."
+      "side-menu side-menu ."
+      ". sub-title ."
+      `,
+    },
+  },
+
+  'paddingTop': '50px',
+  '@supports (display: grid)': {
+    // 'grid-template-columns': '450px auto auto',
+    'grid-template-rows': 'auto',
+    display: 'grid',
+    gridGap: 5,
+    gridTemplateAreas: `
+    "title"
+    "sub-title"
+    "side-menu"
+    `,
+  },
+
+});
+
+const SideMenu = glamorous.div({
+  'gridArea': 'side-menu',
+  'fontSize': '3vmax;',
+  'color': '#FFFFFF ',
+});
+
 
 const Title = glamorous.h1({
   'marginTop': '0px',
-  'paddingLeft': '535px',
-  'paddingTop': '70px',
+  'marginBottom': '0px',
+  // 'marginTop': '0px',
+  // 'paddingLeft': '535px',
+  // 'paddingTop': '70px',
+  // 'marginBottom': '0px',
   'fontFamily': "'Pacifico', cursive",
-  'fontSize': '100px',
-  'color': '#0a1429',
-  'marginBottom': '0px'
+  'fontSize': '4.7vmax;',
+  'color': '#2C2422',
+  'textShadow': '4px 1px 4px #FFFFFF',
+  'gridArea': 'title',
+  'justifySelf': 'center'
 })
 const SubTitle = glamorous.h2({
   'marginTop': '0px',
-  'paddingTop': '550px',
-  'paddingLeft': '900px',
+  'marginBottom': '0px',
+  // 'paddingTop': '550px',
+  // 'paddingLeft': '900px',
   'fontFamily': "'Pacifico', cursive",
-  'fontSize': '100px',
-  'color': '#0a1429',
+  'fontSize': '4.7vmax;',
+  'color': '#0f1e3e',
+  'textShadow': '4px 1px 4px #FFFFFF',
+  'gridArea': 'sub-title',
+  'justifySelf': 'center'
 })
 
 export default getRouteProps(({ title, subTitle, backgroundImageUrl }) => (
   <Background backgroundImageUrl = {backgroundImageUrl}>
-    <Title>{title}</Title>
-    <SubTitle>{subTitle}</SubTitle>
+    <Grid>
+      <Title>{title}</Title>
+      <SubTitle>{subTitle}</SubTitle>
+      <SideMenu>
+        <ul>
+          <li>Menu</li>
+          <li>Menu</li>
+          <li>Menu</li>
+        </ul>
+      </SideMenu>
+    </Grid>
   </Background>
 ))
