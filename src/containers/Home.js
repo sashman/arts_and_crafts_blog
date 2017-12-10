afece224c26015bba12de6a93b8c8d4b2df995fb
Dave Clement <davejclement@gmail.com>
2017-12-10 01:12:40 +0000

    All DCs hard work

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -8,6 +8,16 @@ const Background = glamorous.div( ({backgroundImageUrl}) => ({
   background: "url(" + backgroundImageUrl + ") center/cover"
 }))
 
+const Overlay = glamorous.div({
+  position: 'absolute',
+  top: '0',
+  left: '0',
+  right: '0',
+  bottom: '0',
+  background: 'black',
+  opacity: '0.3',
+})
+
 const Grid = glamorous.div({
 
   'marginTop': '0px',
@@ -37,9 +47,19 @@ const Grid = glamorous.div({
   }
 })
 
+
+const ContentWrap = glamorous.div({
+'position': 'absolute',
+'top': '0',
+'left': '200px',
+'right': '200px',
+'maxWidth': '100%',
+'padding': '50px 200px 50px',
+})
+
 const SideMenu = glamorous.div({
+  'zIndex': '100',
   'gridArea': 'side-menu',
-  'fontSize': '3vmax',
   'color': '#FFFFFF ',
   'position': 'fixed',
   'top': '0',
@@ -56,27 +76,30 @@ const SideMenu = glamorous.div({
 const PostList = glamorous.ul({
   'listStyleType': 'none',
   'margin': '0',
-  'padding': '0'
+  'padding': '0',
+  'fontSize': '1.5em',
+  'width': '200px',
 })
 
 const PostListItem = glamorous.li({
-  'backgroundColor': 'black',
+  'backgroundColor': 'rgba(0,0,0,0.3)',
 })
 
 const PostLink = glamorous.a({
   'fontFamily': "'Open Sans', sans-serif",
-  'fontSize': '4vmax',
+  'display': 'inline-block',
   'color': 'white',
+  'padding': '5px 15px',
   // 'padding': '1vmax'
 })
 
 const Title = glamorous.h1({
   'marginTop': '0px',
-  '@media screen and (orientation : landscape)': {
-    'marginBottom': '50vh',
-  },
+  // '@media screen and (orientation : landscape)': {
+  //   'marginBottom': '50vh',
+  // },
   'fontFamily': "'Pacifico', cursive",
-  'fontSize': '4.7vmax',
+  'fontSize': '5em',
   'color': '#2C2422',
   'textShadow': '4px 1px 4px #FFFFFF',
   'gridArea': 'title',
@@ -87,27 +110,34 @@ const SubTitle = glamorous.h2({
   'marginTop': '0px',
   'marginBottom': '0px',
   'fontFamily': "'Pacifico', cursive",
-  'fontSize': '4.7vmax',
+  'fontSize': '5em',
   'color': '#0f1e3e',
   'textShadow': '4px 1px 4px #FFFFFF',
   'gridArea': 'sub-title',
   'justifySelf': 'right'
 })
 
-export default getRouteProps(({ title, subTitle, backgroundImageUrl, postTitles }) => (
-  <Background backgroundImageUrl = {backgroundImageUrl}>
 
+
+
+export default getRouteProps(({ title, subTitle, backgroundImageUrl, postTitles }) => (
+<div>
+  <SideMenu>
+    <PostList>
+      {postTitles.map(({title, slug}) => (
+        <PostListItem>
+          <PostLink>{title}</PostLink>
+        </PostListItem>
+      ))}
+    </PostList>
+  </SideMenu>
+
+  <Background backgroundImageUrl = {backgroundImageUrl}>
+  <Overlay></Overlay>
+    <ContentWrap>
       <Title>{title}</Title>
       <SubTitle>{subTitle}</SubTitle>
-      <SideMenu>
-        <PostList>
-          {postTitles.map(({title, slug}) => (
-            <PostListItem>
-              <PostLink>{title}</PostLink>
-            </PostListItem>
-          ))}
-        </PostList>
-      </SideMenu>
-
+    </ContentWrap>
   </Background>
+</div>
 ))
