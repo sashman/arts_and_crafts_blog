2e729c8311c875844995e4840de2b15c98fed9d0
sash man <sashman90@gmail.com>
2017-12-23 19:38:02 +0000

    Placing a gradient on top of the background picture

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import { getSiteProps, getRouteProps } from 'react-static'
+import {getSiteProps, getRouteProps} from 'react-static'
 import glamorous from 'glamorous'
 
-const Background = glamorous.div( ({backgroundImageUrl}) => ({
+const goldColour = '#C19B53'
+const blackColour = 'black'
+const whiteColour = 'white'
+const darkBlueColour = '#0D0D16'
+const blueColour = '#0A142B'
+
+const fontFamily = "'Pacifico', cursive"
+const fontShadow = `-2px 0 ${whiteColour}, 0 2px ${whiteColour}, 2px 0 ${whiteColour}, 0 -2px ${whiteColour}`
+
+const Background = glamorous.div(({backgroundImageUrl}) => ({
   width: "100%",
   height: "100vh",
   background: "url(" + backgroundImageUrl + ") center/cover"
 }))
+
+const BackgroundOverlay = glamorous.div({width: "100%", height: "100%", 'background': `linear-gradient(to right, ${blueColour} 64%, rgba(0,0,0,0))`})
 
 const Overlay = glamorous.div({
   position: 'absolute',
@@ -15,46 +26,17 @@ const Overlay = glamorous.div({
   right: '0',
   bottom: '0',
   background: 'black',
-  opacity: '0.3',
+  opacity: '0.3'
 })
-
-const Grid = glamorous.div({
-
-  'marginTop': '0px',
-  'paddingTop': '50px',
-  '@media screen and (orientation : landscape)': {
-    '@supports (display: grid)': {
-      'gridTemplateRows': 'auto',
-      display: 'grid',
-      gridGap: 5,
-      gridTemplateAreas: `
-      "title ."
-      ". ."
-      "sub-title ."
-      `,
-    },
-  },
-
-  '@supports (display: grid)': {
-    'gridTemplateRows': 'auto',
-    display: 'grid',
-    gridGap: 5,
-    gridTemplateAreas: `
-    "title"
-    "sub-title"
-    "."
-    `,
-  }
-})
-
 
 const ContentWrap = glamorous.div({
-'position': 'absolute',
-'top': '0',
-'left': '200px',
-'right': '200px',
-'maxWidth': '100%',
-'padding': '50px 200px 50px',
+  'position': 'absolute',
+  'top': '0',
+  'left': '200px',
+  'right': '200px',
+  'maxWidth': '100%',
+  'padding': '50px 200px 50px',
+  'textAlign': 'centre'
 })
 
 const SideMenu = glamorous.div({
@@ -65,79 +47,57 @@ const SideMenu = glamorous.div({
   'top': '0',
   'bottom': '0',
   'left': '0',
-  'display':'flex',
-  'flex-direction':'column',
-  'justify-content':'center',
-  'overflow':'auto',
-
-  // 'height': "50vh",
+  'display': 'flex',
+  'flex-direction': 'column',
+  'justify-content': 'center',
+  'overflow': 'auto'
 })
 
-const PostList = glamorous.ul({
-  'listStyleType': 'none',
-  'margin': '0',
-  'padding': '0',
-  'fontSize': '1.5em',
-  'width': '200px',
-})
+const PostList = glamorous.ul({'listStyleType': 'none', 'margin': '0', 'padding': '0', 'fontSize': '1.5em', 'width': '200px'})
 
-const PostListItem = glamorous.li({
-  'backgroundColor': 'rgba(0,0,0,0.3)',
-})
+const PostListItem = glamorous.li({'backgroundColor': 'rgba(0,0,0,0.3)'})
 
-const PostLink = glamorous.a({
-  'fontFamily': "'Open Sans', sans-serif",
-  'display': 'inline-block',
-  'color': 'white',
-  'padding': '5px 15px',
-  // 'padding': '1vmax'
-})
+const PostLink = glamorous.a({'fontFamily': "'Open Sans', sans-serif", 'display': 'inline-block', 'color': 'white', 'padding': '5px 15px'})
 
 const Title = glamorous.h1({
-  'marginTop': '0px',
-  // '@media screen and (orientation : landscape)': {
-  //   'marginBottom': '50vh',
-  // },
-  'fontFamily': "'Pacifico', cursive",
+  'margin': '0px',
+  'fontFamily': fontFamily,
   'fontSize': '5em',
-  'color': '#2C2422',
-  'textShadow': '4px 1px 4px #FFFFFF',
-  'gridArea': 'title',
-  'justifySelf': 'right'
+  'color': darkBlueColour,
+  'textShadow': fontShadow,
+  'horizontalAlign': 'middle'
 })
 
 const SubTitle = glamorous.h2({
   'marginTop': '0px',
   'marginBottom': '0px',
-  'fontFamily': "'Pacifico', cursive",
+  'marginLeft': 'auto',
+  'marginRight': 'auto',
+  'fontFamily': fontFamily,
   'fontSize': '5em',
-  'color': '#0f1e3e',
-  'textShadow': '4px 1px 4px #FFFFFF',
-  'gridArea': 'sub-title',
-  'justifySelf': 'right'
+  'color': darkBlueColour,
+  'textShadow': fontShadow,
+  'horizontalAlign': 'middle'
 })
 
-
-
-
-export default getRouteProps(({ title, subTitle, backgroundImageUrl, postTitles }) => (
-<div>
+export default getRouteProps(({title, subTitle, backgroundImageUrl, postTitles}) => (<div>
   <SideMenu>
     <PostList>
-      {postTitles.map(({title, slug}) => (
-        <PostListItem>
+      {
+        postTitles.map(({title, slug}) => (<PostListItem>
           <PostLink>{title}</PostLink>
-        </PostListItem>
-      ))}
+        </PostListItem>))
+      }
     </PostList>
   </SideMenu>
 
-  <Background backgroundImageUrl = {backgroundImageUrl}>
-  <Overlay></Overlay>
-    <ContentWrap>
-      <Title>{title}</Title>
-      <SubTitle>{subTitle}</SubTitle>
-    </ContentWrap>
+  <Background backgroundImageUrl={backgroundImageUrl}>
+    <Overlay></Overlay>
+    <BackgroundOverlay>
+      <ContentWrap>
+        <Title>{title}</Title>
+        <SubTitle>{subTitle}</SubTitle>
+      </ContentWrap>
+    </BackgroundOverlay>
   </Background>
-</div>
-))
+</div>))
