fce1bf3bab6d93a70e445dbb0d7a9206bee3444d
sash man <sashman90@gmail.com>
2018-01-20 17:05:05 +0000

    Toggling side menu via burger button

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -18,7 +18,11 @@ const Background = glamorous.div(({ backgroundImageUrl }) => ({
   background: `url(${backgroundImageUrl}) right/cover`,
 }))
 
-const BackgroundOverlay = glamorous.div({ width: '100%', height: '100%', background: `linear-gradient(to right, ${blueColour} 64%, rgba(0,0,0,0))` })
+const BackgroundOverlay = glamorous.div({
+  width: '100%',
+  height: '100%',
+  background: `linear-gradient(to right, ${blueColour} 64%, rgba(0,0,0,0))`,
+})
 
 const Overlay = glamorous.div({
   position: 'absolute',
@@ -41,44 +45,55 @@ const ContentWrap = glamorous.div({
   maxWidth: '100%',
 })
 
-const Nav = glamorous.nav({
-  backgroundColor: 'rgba(0,0,0,0)',
-  height: '40px',
-  width: 'auto',
-})
+const Nav = glamorous.nav({ backgroundColor: 'rgba(0,0,0,0)', height: '40px', width: 'auto' })
 
-const SideMenu = glamorous.div({
-  zIndex: '100',
-  gridArea: 'side-menu',
-  color: '#FFFFFF',
-  position: 'fixed',
-  top: '0',
-  bottom: '0',
-  [mediaQueries.phone.portrait]: {
-    display: 'fixed',
-    left: '-200px',
+const SideMenu = glamorous.div(
+  {
+    zIndex: '99',
+    gridArea: 'side-menu',
+    color: '#FFFFFF',
+    position: 'fixed',
+    top: '0',
+    bottom: '0',
+    [mediaQueries.phone.portrait]: {
+      display: 'fixed',
+      left: '-200px',
+    },
+    left: '0',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    overflow: 'auto',
   },
-  left: '0',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  overflow: 'auto',
-})
+  props => ({
+    [mediaQueries.phone.portrait]: {
+      display: 'fixed',
+      left: props.hidden ? '-200px' : '0px',
+    },
+  }),
+)
 
 const PostList = glamorous.ul({ listStyleType: 'none', margin: '0', padding: '0', width: '200px' })
 
 const PostListItem = glamorous.li({ backgroundColor: 'rgba(0,0,0,0.3)' })
 
-const PostLink = glamorous.a({ fontFamily: "'Open Sans', sans-serif", display: 'inline-block', color: 'white', padding: '5px 15px' })
+const PostLink = glamorous.a({
+  fontFamily: "'Open Sans', sans-serif",
+  display: 'inline-block',
+  color: 'white',
+  padding: '5px 15px',
+})
 
 const BugerButton = glamorous.button({
   backgroundColor: 'rgba(0,0,0,0)',
   paddingLeft: '.3em',
   color: whiteColour,
   fontSize: '3em',
-  border: 'none',
   position: 'fixed',
   left: '-200px',
+  zIndex: 100,
+  border: 'none',
+  outline: 'none',
   [mediaQueries.phone.portrait]: {
     left: '0px',
   },
@@ -114,27 +129,43 @@ const SubTitle = glamorous.h2({
   horizontalAlign: 'middle',
 })
 
+class NavWrap extends React.Component {
+  state = {
+    hidden: true,
+  }
+  handleClick = e => {
+    e.preventDefault()
+    this.setState({ hidden: !this.state.hidden })
+  }
+  render () {
+    return (
+      <Nav>
+        <BugerButton onClick={this.handleClick}>=</BugerButton>
+        <SideMenu>
+          <PostList hidden={this.state.hidden}>
+            {this.props.postTitles.map(({ title, slug }) => (
+              <PostListItem key={slug}>
+                <PostLink>{title}</PostLink>
+              </PostListItem>
+            ))}
+          </PostList>
+        </SideMenu>
+      </Nav>
+    )
+  }
+}
+
 export default getRouteProps(({ title, subTitle, backgroundImageUrl, postTitles }) => (
   <div>
     <Background backgroundImageUrl={backgroundImageUrl}>
       <Overlay />
       <BackgroundOverlay>
-        <Nav>
-          <BugerButton>=</BugerButton>
-          <SideMenu>
-            <PostList>
-              {
-                postTitles.map(({ title, slug }) => (<PostListItem key={slug}>
-                  <PostLink>{title}</PostLink>
-                </PostListItem>))
-              }
-            </PostList>
-          </SideMenu>
-        </Nav>
+        <NavWrap postTitles={postTitles} />
         <ContentWrap>
           <Title>{title}</Title>
           <SubTitle>{subTitle}</SubTitle>
         </ContentWrap>
       </BackgroundOverlay>
     </Background>
-  </div>))
+  </div>
+))
